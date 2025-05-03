c0b91275a354234592fe97e22d2ccf4854b5d56f
creativus9 <decornove9@gmail.com>
2025-05-03 17:00:44 -0300

    Update compose_dxf.py
    git add compose_dxf.py
    git commit -m "Corrige centralização dos DXFs com base no canto inferior esquerdo da base"
    git push origin main

--- compose_dxf.py
+++ compose_dxf.py
@@ -1,37 +1,55 @@
 import ezdxf
-from ezdxf.addons.importer import Importer
+import os
 from google_drive import baixar_arquivo_drive
 
-# Posições fixas
-posicoes = [
-    (99.5, 113.9), (253.0, 113.9), (406.5, 113.9), (560.0, 113.9), (713.5, 113.9), (867.0, 113.9),
-    (99.5, 311.7), (253.0, 311.7), (406.5, 311.7), (560.0, 311.7), (713.5, 311.7), (867.0, 311.7),
-    (99.5, 509.5), (253.0, 509.5), (406.5, 509.5), (560.0, 509.5), (713.5, 509.5), (867.0, 509.5),
-]
+# posições fixas definidas pelo usuário
+COORDENADAS = {
+    1: (99.5, 113.9),
+    2: (253.0, 113.9),
+    3: (406.5, 113.9),
+    4: (560.0, 113.9),
+    5: (713.5, 113.9),
+    6: (867.0, 113.9),
+    7: (99.5, 311.7),
+    8: (253.0, 311.7),
+    9: (406.5, 311.7),
+    10: (560.0, 311.7),
+    11: (713.5, 311.7),
+    12: (867.0, 311.7),
+    13: (99.5, 509.5),
+    14: (253.0, 509.5),
+    15: (406.5, 509.5),
+    16: (560.0, 509.5),
+    17: (713.5, 509.5),
+    18: (867.0, 509.5),
+}
 
-def compor_dxf_com_base(lista, output_path):
+def compor_dxf_com_base(lista_arquivos, caminho_saida):
     base_path = baixar_arquivo_drive("BASE.DXF")
-    doc_final = ezdxf.readfile(base_path)
-    msp_final = doc_final.modelspace()
+    doc_base = ezdxf.readfile(base_path)
+    msp_base = doc_base.modelspace()
 
-    for item in lista:
-        if item.posicao < 1 or item.posicao > 18:
-            continue
-
-        x, y = posicoes[item.posicao - 1]
+    for item in lista_arquivos:
         arq_path = baixar_arquivo_drive(item.nome, subpasta="arquivos padronizados")
-        doc = ezdxf.readfile(arq_path)
-        msp = doc.modelspace()
+        doc_insert = ezdxf.readfile(arq_path)
+        msp_insert = doc_insert.modelspace()
 
-        importer = Importer(doc, doc_final)
-        importer.import_modelspace()
-        importer.finalize()
+        bbox = msp_insert.bbox()
+        largura = bbox.size.x
+        altura = bbox.size.y
 
-        for e in msp_final:
-            try:
-                if e.dxf.owner == msp_final.dxf.handle:
-                    e.translate(x, y)
-            except Exception:
-                continue
+        centro_x = bbox.center.x
+        centro_y = bbox.center.y
 
-    doc_final.saveas(output_path)
+        destino_x, destino_y = COORDENADAS[item.posicao]
+
+        deslocamento_x = destino_x - centro_x
+        deslocamento_y = destino_y - centro_y
+
+        # copia as entidades com deslocamento calculado
+        for e in msp_insert:
+            nova_entidade = e.copy()
+            nova_entidade.translate(dx=deslocamento_x, dy=deslocamento_y)
+            msp_base.add_entity(nova_entidade)
+
+    doc_base.saveas(caminho_saida)
